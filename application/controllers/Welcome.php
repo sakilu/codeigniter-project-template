534f848657f8070ea4ffa78c4e68148afc32f3b2
sakilu <sakilu@gmail.com>
2015-05-24 10:13:03 +0800

    更改設定檔案

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,12 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-
-    /**
-     *
-     */
 	public function index()
 	{
-        phpinfo();
+        echo "Hello world";
 	}
+
 }
