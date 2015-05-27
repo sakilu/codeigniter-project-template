e1d9603018ada1c1ca21c2edf7345c39ce39d195
sakilu <sakilu@gmail.com>
2015-05-27 10:50:51 +0800

    docker script modify

---
+++ script/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+web_root=/var/www/wedding
+docker run --name marry -itd -p 80:80 -v $web_root:$web_root -v $web_root"/docker/nginx":/etc/nginx -v $web_root"/docker/php5":/etc/php5 -v $web_root"/docker/root":/root f17199f56bf0 sh /root/go.sh
